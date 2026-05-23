b2108e4a24279dbbf11fbe71e0c6567ed5cb4e21
scarrascoore <scarrascoore@gmail.com>
2026-05-23 12:06:53 -0500

    creando el proyecto API REST para la gestión de tareas mediante Spring Boot

---
+++ src/main/java/com/portfolio/taskflow/TaskFlowApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.taskflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFlowApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskFlowApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/taskflow/TaskFlowApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskFlowApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
